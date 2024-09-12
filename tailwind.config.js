482f3d2a3fe226773ef85b4324c5d669cc9a78c1
Ezii54 <alunheadshoter@gmail.com>
2024-09-13 02:36:16 +0700

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+   content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+   ],
+   theme: {
+      extend: {
+         colors: {
+            background: "var(--background)",
+            foreground: "var(--foreground)",
+            color: {
+               primary: "#0d1117",
+               secondary: "#161b22",
+               accent: "#77bdfb",
+               light: "#ecf2f8",
+               dark: "#000000",
+            },
+         },
       },
-    },
-  },
-  plugins: [],
+   },
+   plugins: [],
 };
